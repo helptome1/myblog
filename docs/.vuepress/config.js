e2a8934726b24f7442e67e8dc2b6ef80cdd9949b
helptome1 <hzgdeemail@163.com>
2021-09-22 13:17:42 +0800

    修改了一下配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,10 @@ module.exports = {
   description: "我的前端成长日记",
   theme: 'reco',
   themeConfig: {
+    // type设置网站类型
     type: 'blog',
+    //在所有页面中启用自动生成子侧边栏，原 sidebar 仍然兼容
+    subSidebar: 'auto',
     // 博客配置
     blogConfig: {
       category: {
@@ -14,10 +17,11 @@ module.exports = {
         location: 3,     // 在导航栏菜单中所占的位置，默认3
         text: '标签'      // 默认文案 “标签”
       },
-      socialLinks: [     // 信息栏展示社交信息
-        { icon: 'reco-github', link: 'https://github.com/helptome1' },
-      ]
+      // socialLinks: [     // 信息栏展示社交信息
+      //   { icon: 'reco-github', link: 'https://github.com/helptome1' },
+      // ]
     },
+    // 顶部导航栏配置
     nav: [
       {
         "text": "首页",
@@ -46,6 +50,11 @@ module.exports = {
         ]
       }
     ],
+    // 评论插件
+    valineConfig: {
+      appId: '8DW7GjD96TDJ5lPb8IH7zkb9-gzGzoHsz',// your appId
+      appKey: '9rHN0O43CAXPaOayFXAwi8Uz', // your appKey
+    },
     logo: "/logo.png",
     search: true,
     searchMaxSuggestions: 10,
