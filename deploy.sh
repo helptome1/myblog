2a389f87ab9afbc4950c7f26686141104adc687c
Hezg <hzgdeemail@163.com>
2026-09-02 21:06:22 +0800

    chore: deploy 改 HTTPS 推送,忽略 .DS_Store

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:helptome1/blog master
+git push -f https://github.com/helptome1/blog.git master
 
 # 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
